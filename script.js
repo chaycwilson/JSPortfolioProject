103e48ab1876178b868157dca80b673e38fb1d76
chaycwilson <chayvaneck@gmail.com>
2023-03-16 12:10:54 +0200

    Final portfolio project

---
+++ script.js
@@ -0,0 +1,38 @@
+// setting up the objects that need to be used 
+const MainObj = {
+    inspirationalMessage: [ 'Believe in yourself and all that you are. Know that there is something inside you that is greater than any obstacle.',
+                            "Failure is only the opportunity to begin again, this time more intelligently.",
+                            'Every great dream begins with a dreamer. Always remember, you have within you the strength, the patience, and the passion to reach for the stars to change the world.',
+                            'Believe you can and you are halfway there.',
+                            'The only way to succeed is to believe in yourself and your ability to make a difference in the world.',
+                            "When you feel like quitting, remember why you started."
+                        ],
+    dailyTask: [ "Make someone smile today",
+                 "Do some form of exercise",
+                 "Make someone happy today",
+                 "Do something that makes you feel good",
+                 "Do something that makes you feel loved",
+                 "Make a change in your mindset"
+
+    ],
+    jokeOfTheDay: ["What do you call a fish wearing a bowtie? Sofishticated.",
+                   "Why did the tomato turn red? Because it saw the salad dressing!",
+                   "Why did the math book look sad? Because it had too many problems.",
+                   "Why did the scarecrow win an award? Because he was outstanding in his field.",
+                   "What do you call a fake noodle? An impasta.",
+                   "Why do bees hum? Because they don't know the words."
+
+                    ]
+}
+
+//picking at an inspirationalMessage, dailyTask and jokeOfTheDay at random
+const randMessage = (arr) => {
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    return arr[randomIndex];   
+}
+
+
+console.log("Inspirational message: " , randMessage(MainObj.inspirationalMessage));
+console.log("Daily task: " , randMessage(MainObj.dailyTask));
+console.log("Your joke of the day: " , randMessage(MainObj.jokeOfTheDay));
+
